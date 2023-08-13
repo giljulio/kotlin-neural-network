de5fcfbc6a6d35d3156263374cc4bb46c872b6fe
Gil Sinclair-Julio <gil@golightyear.com>
2023-08-13 12:44:58 +0100

    Fix network recreation bug

--- src/commonMain/kotlin/com/giljulio/nn/main.kt
+++ src/commonMain/kotlin/com/giljulio/nn/main.kt
@@ -10,10 +10,13 @@ import com.giljulio.nn.util.randomWeight
 
 
 fun main() {
-    // Create a network with 2 inputs, 1 hidden layer with 2 neurons and 1 output
     val network = createNetwork()
-    val evaluator = MeanSquaredErrorEvaluator()
-    val trainer = Trainer(evaluator, network, iterations = 100000, learningRate = 0.2)
+    val trainer = Trainer(
+        evaluator = MeanSquaredErrorEvaluator(),
+        network = network,
+        iterations = 100000,
+        learningRate = 0.3,
+    )
 
     // Training data for the AND function
     val trainingInputs = listOf(
@@ -29,21 +32,12 @@ fun main() {
         listOf(1.0)
     )
 
-    // train network
     trainer.trainAll(trainingInputs, trainingTargets)
 
     // Test the network
-    val realData = listOf(
-        listOf(0.0, 0.0),
-        listOf(0.0, 1.0),
-        listOf(1.0, 0.0),
-        listOf(1.0, 1.0),
-        listOf(2.0, 2.0),
-        listOf(3.0, 7.0),
-    )
-    realData.forEachIndexed { i, data ->
-        val output = network.forward(data)
-        println("Inputs: $data, Expected: ${realData[i]}, Got: $output")
+    for (index in trainingInputs.indices) {
+        val output = network.forward(trainingInputs[index])
+        println("Inputs: ${trainingInputs[index]}, Expected: ${trainingTargets[index]}, Got: $output")
     }
 }
 
